11efbf5f87afbc46bdf58a10389643f1f1ab6111
RaIsseMa <bluemanbusiness@gmail.com>
2023-05-15 22:56:21 +0100

    update setup db script

--- setup-test-db.sh
+++ setup-test-db.sh
@@ -4,10 +4,10 @@ superuser_password="testpassword"
 echo "super user password = $superuser_password"
 echo "creating role..."
 
-psql -U "$superuser" -h localhost -d postgres -c "CREATE ROLE testuser WITH LOGIN PASSWORD 'testpassword'"
-psql -U "$superuser" -h localhost -d postgres -c "ALTER ROLE testuser CREATEDB"
+PGPASSWORD="$superuser_password" psql -U "$superuser" -h localhost -d postgres -c "CREATE ROLE testuser WITH LOGIN PASSWORD 'testpassword'"
+PGPASSWORD="$superuser_password" psql -U "$superuser" -h localhost -d postgres -c "ALTER ROLE testuser CREATEDB"
 
 echo "creating test db..."
 
-psql -U testuser -d postgres -c "CREATE DATABASE home_cloud_test"
-psql -U testuser -d home_cloud_test -f db-setup.sql
+PGPASSWORD="$superuser_password" psql -U testuser -d postgres -c "CREATE DATABASE home_cloud_test"
+PGPASSWORD="$superuser_password" psql -U testuser -d home_cloud_test -f db-setup.sql
