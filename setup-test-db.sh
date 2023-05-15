b1597cabbe875c2cd58a209c7d28d7b7852bb5bf
RaIsseMa <bluemanbusiness@gmail.com>
2023-05-15 22:14:34 +0100

    update setup db script

--- setup-test-db.sh
+++ setup-test-db.sh
@@ -1,4 +1,4 @@
-export PGPASSWORD="031900"
+export PGPASSWORD="testpassword"
 
 echo "creating role..."
 
@@ -6,8 +6,6 @@ psql -U postgres -d postgres -c "CREATE ROLE testuser WITH LOGIN PASSWORD 'testp
 psql -U postgres -d postgres -c "ALTER ROLE testuser CREATEDB"
 
 echo "creating test db..."
-
-export PGPASSWORD="testpassword"
 
 psql -U testuser -d postgres -c "CREATE DATABASE home_cloud_test"
 psql -U testuser -d home_cloud_test -f db-setup.sql
